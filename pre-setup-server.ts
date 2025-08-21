6670463c849c36eb0e87329598252e498b43251d
MiraSayon <mirasayon@ya.ru>
2025-08-21 08:27:12 +0500

    updated deps; prepare for merge shared repo to this project;

---
+++ pre-setup-server.ts
@@ -0,0 +1,16 @@
+import { Logger } from "log-it-colored";
+import { readdir } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+async function main() {
+    const _dirname = dirname(fileURLToPath(import.meta.url));
+    const folderPath = join(_dirname, "package");
+    const _folder = await readdir(folderPath, { encoding: "utf-8" });
+    const packageName = _folder[0];
+    if (!packageName) {
+        throw new Error("No tarball in `./package` folder");
+    }
+    console.clear();
+    Logger.violet(`npm install http://192.168.0.105:5641/${packageName}`);
+}
+await main();
---
+++ prepack.ts
@@ -0,0 +1,21 @@
+import { Logger } from "log-it-colored";
+import { env } from "node:process";
+import { existsSync } from "node:fs";
+import { mkdir, rm } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+if (env.XPACK !== "true") {
+    Logger.red("You actually need to run `npm run xpack` for that");
+    process.exit(1);
+}
+
+if (env.XPACK === "true") {
+    Logger.violet("Packing tarball...");
+    const _dirname = dirname(fileURLToPath(import.meta.url));
+    const packagePath = join(_dirname, "package");
+    if (existsSync(packagePath)) {
+        await rm(packagePath, { recursive: true, force: true });
+    }
+    await mkdir(packagePath);
+}
