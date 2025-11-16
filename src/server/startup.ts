aad1c6090710f5b5fcf1cc5c58d3c40b7cd9ed22
Miralisher <mirasayon@ya.ru>
2025-11-16 18:35:57 +0500

    opt-out of image rendering. remove the package of image rendering.

--- src/server/startup.ts
+++ src/server/startup.ts
@@ -1,26 +1,10 @@
-import console from "node:console";
-console.clear();
-console.time("in");
-import terminalImage from "terminal-image";
+import { clear, time, timeEnd } from "node:console";
+clear();
+time("in");
 import { test_db } from "#/providers/adapter.js";
 import { prisma } from "#/providers/database-connect.js";
 import { startThisServer } from "#/server/class.js";
-import { PathsConfig } from "#/configs/paths.config.js";
-import { join } from "path";
-import { EnvConfig } from "#/configs/environment-variables.js";
 
 await startThisServer();
 await test_db(prisma);
-console.timeEnd("in");
-
-if (EnvConfig.is_prod) {
-    console.log(
-        await terminalImage.file(join(PathsConfig.static, "selena-mlbb-art.jpg"), {
-            height: "60%",
-            width: "60%",
-            preserveAspectRatio: true,
-        }),
-    );
-} else {
-    console.log("...");
-}
+timeEnd("in");
